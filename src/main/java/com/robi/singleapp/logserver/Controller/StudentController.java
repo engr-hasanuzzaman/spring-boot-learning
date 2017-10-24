a0dd75cbdd7f9e26d247154382a0032d6a464806
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-10-24 10:45:18 +0600

    add right annotation for routing

--- src/main/java/com/robi/singleapp/logserver/Controller/StudentController.java
+++ src/main/java/com/robi/singleapp/logserver/Controller/StudentController.java
@@ -2,14 +2,20 @@ package com.robi.singleapp.logserver.Controller;
 
 import com.robi.singleapp.logserver.Entity.Student;
 import com.robi.singleapp.logserver.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
     private StudentService studentService;
 
+    @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents(){
         return this.studentService.getAllStudents();
     }
--- src/main/java/com/robi/singleapp/logserver/Service/StudentService.java
+++ src/main/java/com/robi/singleapp/logserver/Service/StudentService.java
@@ -2,12 +2,14 @@ package com.robi.singleapp.logserver.Service;
 
 import com.robi.singleapp.logserver.Dao.StudentDao;
 import com.robi.singleapp.logserver.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
 @Service
 public class StudentService {
+    @Autowired
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
